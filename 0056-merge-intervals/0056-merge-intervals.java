d6023dae7567f98682cba832e8a166d978ece6bb
pardhasaradhi-sde <seerapupardhu123@gmail.com>
2025-06-07 13:03:13 +0530

    Time: 8 ms (86.67%), Space: 46.8 MB (39.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-       Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-       List<int[]> l1=new ArrayList<>();
-       l1.add(new int[]{intervals[0][0],intervals[0][1]});
-       for(int i=0;i<intervals.length;i++)
-       {
-        int[] last=l1.get(l1.size()-1);
-        int curr[]=intervals[i];
-        if(last[1]>=curr[0])
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int []> l1=new ArrayList<>();
+        l1.add(new int[] {intervals[0][0],intervals[0][1]});
+        for(int i=1;i<intervals.length;i++)
         {
-            last[1]=Math.max(last[1],curr[1]);
-        }else{
-            l1.add(new int[]{curr[0],curr[1]});
+            int last[]=l1.get(l1.size()-1);
+            int curr[]=intervals[i];
+            if(curr[0]<=last[1])
+            {
+                last[1]=Math.max(curr[1],last[1]);
+            }
+            else{
+                l1.add(new int[] {curr[0],curr[1]});
+            }
         }
-       }
-       return l1.toArray(new int[l1.size()][]);
+        int[][] arr=new int[l1.size()][2];
+        for(int i=0;i<l1.size();i++)
+        {
+            arr[i]=l1.get(i);
+        }
+        return arr;
     }
 }
