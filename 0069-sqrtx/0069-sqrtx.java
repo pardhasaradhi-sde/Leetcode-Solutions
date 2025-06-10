a40fcbc29095dfa81148387a270f38fa139cc08e
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-06-10 09:08:05 +0530

    Time: 1 ms (87.33%), Space: 41 MB (54.57%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0||x==1)
+        {
+            return x;
+        }
+        int low=1;
+        int high=x/2;
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            long square=(long)mid*mid;
+            if(square==x)
+            {
+                return mid;
+            }
+            else if(square<x)
+            {
+                low=mid+1;
+                ans=mid;
+            }
+            else{
+                high=mid-1;
+
+            }
+        }
+        return ans;
+    }
+}
