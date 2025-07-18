5bf0ff5eb8c048b50745ddf79c29c4d9decce907
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-07-18 19:04:36 +0530

    Time: 6 ms (47.91%), Space: 60 MB (67.78%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,27 +9,50 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        Stack<Integer> s=new Stack<>();
+    public ListNode middle(ListNode head)
+    {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode reverse(ListNode head)
+    {
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
         ListNode temp=head;
-        int size=0;
+        ListNode prev=null;
         while(temp!=null)
         {
-            size++;
-            s.push(temp.val);
-            temp=temp.next;
+            ListNode next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
         }
-        temp=head;
-        for(int i=1;i<(size/2)+1;i++)
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null)
         {
-            if(temp.val!=s.peek())
+            return true;
+        }
+        ListNode middle=middle(head);
+        ListNode lefthead=head;
+        ListNode righthead=reverse(middle);
+        while( righthead!=null)
+        {
+            if(lefthead.val!=righthead.val)
             {
                 return false;
             }
-            else{
-                temp=temp.next;
-                s.pop();
-            }
+            lefthead=lefthead.next;
+            righthead=righthead.next;
         }
         return true;
     }
