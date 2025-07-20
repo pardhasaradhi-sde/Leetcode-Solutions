d5d252cdd7de1089b2ac8d913815df7b5e574ecf
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-07-20 15:16:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,18 @@
-class Solution {
-    public double myPow(double x, int n) {
-       long exp=n;
-       if(exp<0)
-       {
-        x=1/x;
-        exp=-exp;
-       }
-       return pow(x,exp);
-    }
-    double pow(double x,long n)
-    {
-        if(n==0)
-        {
-            return 1;
-        }
-        double half=pow(x,n/2);
-        if(n%2==0)
-        {
-            return half*half;
-        }
-        else{
-            return half*half*x; 
+    class Solution {
+        public double myPow(double x, int n) {
+            long N=n;
+            if(N==0)
+            {
+                return 1;
+            }
+            if(N<0)
+            {
+                return 1/myPow(x,(int)-N);
+            }
+            if(N==1)
+            {
+                return x;
+            }
+            return x*myPow(x,(int)N-1);
         }
     }
-}
