772b7912ff6cd478081e971a86096c8c1f78a382
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-07-21 11:05:17 +0530

    Time: 0 ms (100%), Space: 41.2 MB (26.12%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,18 +1,7 @@
 class Solution {
+    
     public boolean isPowerOfTwo(int n) {
-        if(n<=0)
-        {
-            return false;
-        }
-        String binary=Integer.toBinaryString(n);
-        int count=0;
-        for(int i=0;i<binary.length();i++)
-        {
-            if(binary.charAt(i)=='1')
-            {
-                count++;
-            }
-        }
-        return (count==1)?true:false;
+       
+        return (n>0) && ((n&(n-1))==0);
     }
 }
