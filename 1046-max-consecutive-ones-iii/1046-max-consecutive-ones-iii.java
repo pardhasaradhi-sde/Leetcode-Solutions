8845619519335e32fcfb135f753374be0b54ea56
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-07-25 15:51:42 +0530

    Time: 3 ms (93.93%), Space: 53.5 MB (6.41%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left=0;
+        int zerocount=0;
+        int count=0;
+        int maxcount=0;
+        for(int right=0;right<nums.length;right++)
+        {
+            if(nums[right]==0)
+            {
+                zerocount++;
+            }
+            while(zerocount>k )
+            {
+                if(nums[left]==0)
+                {
+                    zerocount--;
+                }
+                
+                left++;
+            }
+            maxcount=Math.max(right-left+1,maxcount);
+        }
+        return maxcount;
+    }
+}
