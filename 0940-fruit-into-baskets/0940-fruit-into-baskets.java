8fdbdeb57f9acbf013fb02d7aa76ea386caae878
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-07-25 16:27:59 +0530

    Time: 53 ms (24.13%), Space: 55.2 MB (51.67%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer,Integer> l1=new HashMap<>();
+        int left=0;
+        int right=0;
+        int maxlen=0;
+        while(right<fruits.length)
+        {
+           l1.put(fruits[right],l1.getOrDefault(fruits[right],0)+1);
+           while(l1.size()>2)
+           {
+            l1.put(fruits[left],l1.getOrDefault(fruits[left],0)-1);
+            if(l1.get(fruits[left])==0)
+            {
+                l1.remove(fruits[left]);
+            }
+            left++;
+           }
+           maxlen=Math.max(maxlen,right-left+1);
+           right++;
+        }
+       return maxlen;
+        
+    }
+}
