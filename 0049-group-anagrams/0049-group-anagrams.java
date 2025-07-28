8af2d2426cc37172025dc2bab53922030b52c1d3
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-07-29 04:31:07 +0530

    Time: 17 ms (25.92%), Space: 48.1 MB (34.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,14 +3,22 @@ class Solution {
         Map<String,List<String>> l1=new HashMap<>();
         for(String str:strs)
         {
-            char c[]=str.toCharArray();
-            Arrays.sort(c);
-            String sortedword=new String(c);
-            if(!l1.containsKey(sortedword))
+            int count[]=new int[26];
+            for(char c:str.toCharArray())
             {
-                l1.put(sortedword,new ArrayList<>());
+               count[c-'a']++;
+            } 
+            StringBuilder sb=new StringBuilder();
+            for( int freq:count)
+            {
+                sb.append(freq).append('#');
             }
-            l1.get(sortedword).add(str);
+            String key=sb.toString();
+            if(!l1.containsKey(key))
+            {
+                l1.put(key,new ArrayList<>());
+            }
+            l1.get(key).add(str);
         }
         return new ArrayList<>(l1.values());
     }
