1cdac6954a87b22e7305c280ed5425688308752d
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-10 16:39:59 +0530

    Time: 0 ms (100%), Space: 40.7 MB (67.89%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.java
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -1,39 +1,24 @@
 class Solution {
-    public boolean ispowerof2(int n)
+    private int[] countdigits(int n)
     {
-        return (n>0) && ((n&(n-1))==0);
+        int num=n;
+        int count[]=new int[10];
+        while(num>0)
+        {
+            count[num%10]++;
+            num=num/10;
+        }   
+        return count;
     }
-    public boolean generatepermutation(char[] arr,int idx)
-    {
-        if(idx==arr.length)
+    public boolean reorderedPowerOf2(int n) {
+        int target[]=countdigits(n);
+        for(int i=0;i<31;i++)
         {
-            if(arr[0]=='0')
-            {
-                return false;
-            }
-            int num=Integer.parseInt(new String(arr));
-            return ispowerof2(num);
-        }
-        for(int i=idx;i<arr.length;i++)
-        {
-            swap(arr,i,idx);
-            if(generatepermutation(arr,idx+1))
+            if(Arrays.equals(target,countdigits(1<<i)))
             {
                 return true;
             }
-            swap(arr,i,idx);
         }
         return false;
-    }
-    public void swap(char arr[],int i,int j)
-    {
-        char temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    public boolean reorderedPowerOf2(int n) {
-        char []arr=String.valueOf(n).toCharArray();
-        return generatepermutation(arr,0);
-        
     }
 }
