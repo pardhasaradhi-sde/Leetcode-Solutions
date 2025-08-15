f360a1def8590499cacf73b0067b4a08c55f7bb1
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-15 10:26:56 +0530

    Time: 0 ms (100%), Space: 40.6 MB (96.12%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,15 +1,29 @@
 class Solution {
+    public boolean ispoweroftwo(int n)
+    {
+        return ((n&(n-1))==0);
+    }
+    public boolean countofsetbits(int n)
+    {
+        int count=0;
+        while(n>0)
+        {
+            if((n&1)!=0)
+            {
+                count++;
+            }
+            n=n>>1;
+        }
+        return count%2==0;
+    }
     public boolean isPowerOfFour(int n) {
         if(n<=0)
         {
             return false;
         }
-        for(int i=0;i<=30;i+=2)
+        if(ispoweroftwo(n) && countofsetbits(n-1))
         {
-            if(n==(1<<i))
-            {
-                return true;
-            }
+            return true;
         }
         return false;
     }
