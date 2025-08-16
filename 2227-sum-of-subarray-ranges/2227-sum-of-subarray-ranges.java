83923543b7f633ecb4e459927c314948c93bfcda
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-16 13:30:17 +0530

    Time: 42 ms (24.86%), Space: 44.7 MB (29.28%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,18 +1,102 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        long sum=0;
-        int n=nums.length;
-        for(int i=0;i<nums.length;i++)
-        {
-            int low=nums[i];
-            int high=nums[i];
-            for(int j=i;j<n;j++)
-            {
-                low=Math.min(low,nums[j]);
-                high=Math.max(high,nums[j]);
-                sum+=(high-low);
+    //Next Smaller Element
+    public static int[] nextSmallerElement(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && nums[st.peek()] >= nums[i]) {
+                st.pop();
             }
+            res[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
         }
-        return sum;
+        return res;
+    }
+
+    //Previous Smaller Element
+    public static int[] prevSmallerElement(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < n; i++) {
+            while(!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
+            }
+            res[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return res;
+    }
+
+    //Next Greater Element
+    public static int[] nextGreaterElement(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            res[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        return res;
+    }
+
+    //Previous Greater Element
+    public static int[] prevGreaterElement(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < n; i++) {
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]) {
+                st.pop();
+            }
+            res[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return res;
+    }
+
+    //sum of subarray minimums
+    public static long sumOfSubarrayMins(int[] nums) {
+        int[] pse = prevSmallerElement(nums);
+        int[] nse = nextSmallerElement(nums);
+        long total = 0;
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++) {
+            long l = i - pse[i];
+            long r = nse[i] - i;
+            total += (l * r * (long) nums[i]);
+        }
+        return total;
+    }
+
+    //sum of subarray maximums
+    public static long sumOfSubarrayMaxs(int[] nums) {
+        int[] pge = prevGreaterElement(nums);
+        int[] nge = nextGreaterElement(nums);
+        long total = 0;
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++) {
+            long l = i - pge[i];
+            long r = nge[i] - i;
+            total += (l * r * (long) nums[i]);
+        }
+        return total;
+    }
+
+    //Final: SUM OF ALL SUBARRAY RANGES
+    public long subArrayRanges(int[] nums) {
+        long min = sumOfSubarrayMins(nums);
+        long max = sumOfSubarrayMaxs(nums);
+        return max - min;
     }
 }
