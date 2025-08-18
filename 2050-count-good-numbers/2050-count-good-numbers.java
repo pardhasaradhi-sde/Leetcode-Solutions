66d00513eb4972d6fa2e6b81fbcebaf0f02541f9
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-18 18:35:59 +0530

    Time: 0 ms (100%), Space: 40.5 MB (51.57%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,22 +1,20 @@
 class Solution {
     final long mod=1000000007;
-    public long recurse(long base,long exp)
+    public long recu(long  a,long  b)
     {
-        if(exp==0)
+        if(b==0)
         {
             return 1;
         }
-        long half=recurse(base,exp/2);
-        if(exp%2==1)
+        long  half=recu(a,b/2);
+        long res=(half*half)%mod;
+        if(b%2==1)
         {
-            return (half*half*base)%mod;
+            res=(res*a)%mod;
         }
-        return (half*half)%mod;
-
+        return res;
     }
     public int countGoodNumbers(long n) {
-        long evencount=(n+1)/2;
-        long oddcount=n/2;
-        return (int)(recurse(5,evencount)*recurse(4,oddcount)%mod);
+       return (int)((recu(5, (n + 1) / 2) * recu(4, n / 2)) % mod);
     }
 }
