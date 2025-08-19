9c978d339c68f464e3b4ca1f3787ea5af878fc18
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-19 07:16:13 +0530

    Time: 3 ms (94.03%), Space: 62.1 MB (29.22%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long count=0;
+        long streak=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                streak++;
+                count+=streak;
+            }
+            else{
+                streak=0;
+            }
+        }
+        return count;
+    }
+}
