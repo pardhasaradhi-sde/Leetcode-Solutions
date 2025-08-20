1e15270396ae5a10a342658d93bfcfcf610df834
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-20 14:48:56 +0530

    Time: 114 ms (19.2%), Space: 46.3 MB (67.15%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -11,27 +11,29 @@ class Solution {
         weight.put('i',3);
         weight.put('o',2);
         weight.put('u',1);
-        PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->weight.get(b)-weight.get(a));
-        Map<Integer,Character> vowelmap=new HashMap<>();
+        List<Character> vowels=new ArrayList<>();
         for(int i=0;i<s.length();i++)
         {
             char c=s.charAt(i);
             if(weight.containsKey(c))
             {
-                vowelmap.put(i,c);
-                pq.offer(c);
+                vowels.add(c);
             }
         }
-        if(vowelmap.isEmpty())
+        if(vowels.isEmpty())
         {
             return s;
         }
+        vowels.sort((a,b)->weight.get(b)-weight.get(a));
         StringBuilder res=new StringBuilder(s);
-        List<Integer> indices=new ArrayList<>(vowelmap.keySet());
-        Collections.sort(indices);
-        for(int i=0;i<indices.size();i++)
+        int vowelindex=0;
+        for(int i=0;i<s.length();i++)
         {
-            res.setCharAt(indices.get(i),pq.poll());
+            char c=s.charAt(i);
+            if(weight.containsKey(c))
+            {
+                res.setCharAt(i,vowels.get(vowelindex++));
+            }
         }
         return res.toString();  
     }
