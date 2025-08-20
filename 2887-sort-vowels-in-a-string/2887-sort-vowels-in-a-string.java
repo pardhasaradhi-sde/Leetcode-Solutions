f6ac05526c4af2817e2c16714f1a0e96d65c7b0c
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-20 14:42:34 +0530

    Time: 302 ms (18.09%), Space: 58 MB (5.33%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,38 +1,38 @@
 class Solution {
     public String sortVowels(String s) {
-        Map<Character,Integer> l1=new HashMap<>();
-        l1.put('A',10);
-        l1.put('E',9);
-        l1.put('I',8);
-        l1.put('O',7);
-        l1.put('U',6);
-        l1.put('a',5);
-        l1.put('e',4);
-        l1.put('i',3);
-        l1.put('o',2);
-        l1.put('u',1);
-        List<Character> s1=new ArrayList<>();
-        Map<Integer,Character> l2=new HashMap<>();
+        Map<Character,Integer> weight=new HashMap<>();
+        weight.put('A',10);
+        weight.put('E',9);
+        weight.put('I',8);
+        weight.put('O',7);
+        weight.put('U',6);
+        weight.put('a',5);
+        weight.put('e',4);
+        weight.put('i',3);
+        weight.put('o',2);
+        weight.put('u',1);
+        PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->weight.get(b)-weight.get(a));
+        Map<Integer,Character> vowelmap=new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
             char c=s.charAt(i);
-            if(l1.containsKey(c))
+            if(weight.containsKey(c))
             {
-                l2.put(i,c);
-                s1.add(c);
+                vowelmap.put(i,c);
+                pq.offer(c);
             }
         }
-        s1.sort((a,b)->l1.get(b)-l1.get(a));
-        List<Integer> change=new ArrayList<>(l2.keySet());
-        Collections.sort(change);
-        StringBuilder res=new StringBuilder(s);
-        for(int i=0;i<change.size();i++)
+        if(vowelmap.isEmpty())
         {
-            res.setCharAt(change.get(i),s1.get(i));
+            return s;
         }
-        return res.toString();
-
-        
-        
+        StringBuilder res=new StringBuilder(s);
+        List<Integer> indices=new ArrayList<>(vowelmap.keySet());
+        Collections.sort(indices);
+        for(int i=0;i<indices.size();i++)
+        {
+            res.setCharAt(indices.get(i),pq.poll());
+        }
+        return res.toString();  
     }
 }
