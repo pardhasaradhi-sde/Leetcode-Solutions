912c7095bd3fb44f82fdf6d9d242df0518e00702
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-23 09:26:06 +0530

    Time: 5 ms (53.16%), Space: 43.5 MB (54.8%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minLength(String s) {
-        StringBuilder s1=new StringBuilder();
+        Stack<Character> s1=new Stack();
         for(int i=0;i<s.length();i++)
         {
-            int n=s1.length();
-            if(n>0 && ((s1.charAt(n-1)=='A' && s.charAt(i)=='B') || (s1.charAt(n-1)=='C' && s.charAt(i)=='D')))
+            int n=s1.size();
+            if(n>0 && ((s1.peek()=='A' && s.charAt(i)=='B') || (s1.peek()=='C' && s.charAt(i)=='D')))
             {
-                s1.deleteCharAt(n-1);
+                s1.pop();
             }
             else{
-                s1.append(s.charAt(i));
+                s1.push(s.charAt(i));
             }
         }
-        return s1.length();
+        return s1.size();
     }
 }
