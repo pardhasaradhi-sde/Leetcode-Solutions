c55ab00c0c83c4ce7adccbdec2437bce2783feb5
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-08-27 07:49:05 +0530

    Time: 0 ms (100%), Space: 40.4 MB (88.03%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.java
@@ -1,21 +1,19 @@
 class Solution {
     public int kthFactor(int n, int k) {
-         List<Integer> factors = new ArrayList<>();
-        List<Integer> lastfact=new ArrayList<>();
-        for (int i = 1; i * i <= n; i++) {
-            if (n % i == 0) {
-                factors.add(i);  // smaller factor
-                if (i!=n/i) {
-                    lastfact.add(n / i); // paired factor
-                }
+       if(k==1)
+       {
+        return 1;
+       }
+       int count=1;
+        for (int i = 2;i<= n; i++) {
+            if (n % i == 0) { 
+                count++;
+            }
+            if(count==k)
+            {
+                return i;
             }
         }
-        Collections.reverse(lastfact);
-        factors.addAll(lastfact);
-        if(k>factors.size())
-        {
-            return -1;
-        }
-        return factors.get(k-1);
+        return -1;
     }
 }
