bc4fef130a42342cab816f779103309b860567ae
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-04 17:44:48 +0530

    Time: 0 ms (100%), Space: 43 MB (26.11%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,32 +14,19 @@
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root) {
-        Stack<TreeNode> prev=new Stack<>();
-        Stack<Integer> depth=new Stack<>();
+    private int depth=0;
+    public void depth1(TreeNode root,int node_depth)
+    {
         if(root==null)
         {
-            return 0;
+            depth=Math.max(depth,node_depth);
+            return;
         }
-        prev.push(root);
-        depth.push(1);
-        int maxdepth=0;
-        while(!prev.isEmpty())
-        {
-            TreeNode ele=prev.pop();
-            int dep=depth.pop();
-            maxdepth=Math.max(dep,maxdepth);
-            if(ele.left!=null)
-            {
-                prev.push(ele.left);
-                depth.push(dep+1);
-            }
-            if(ele.right!=null)
-            {
-                prev.push(ele.right);
-                depth.push(dep+1);
-            }
-        }
-        return maxdepth;
+        depth1(root.left,node_depth+1);
+        depth1(root.right,node_depth+1);
+    }
+    public int maxDepth(TreeNode root) {
+        depth1(root,0);
+        return depth;
     }
 }
