28cf8d385804ed083a2e37fbf593db41e31ad711
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-04 06:56:19 +0530

    Time: 1 ms (34.71%), Space: 40.9 MB (71.18%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.java
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -1,31 +1,7 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
-        int p1=0;
-        int p2=0;
-        while(x!=z)
-        {
-            if(x>z)
-            {
-                x--;
-                p1++;
-            }
-            else{
-                x++;
-                p1++;
-            }
-        }
-        while(y!=z)
-        {
-            if(y>z)
-            {
-                y--;
-                p2++;
-            }
-            else{
-                y++;
-                p2++;
-            }
-        }
+        int p1=Math.abs(z-x);
+        int p2=Math.abs(z-y);
         if(p1>p2)
         {
             return 2;
