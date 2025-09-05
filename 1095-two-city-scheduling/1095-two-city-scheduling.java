88fb41be27111bbcd07c1c7ef9bbbf3fca067f76
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-05 12:39:06 +0530

    Time: 2 ms (86.37%), Space: 41.7 MB (76.04%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.java
+++ 1095-two-city-scheduling/1095-two-city-scheduling.java
@@ -1,15 +1,15 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int cost=0;
-        Arrays.sort(costs,(a,b)->(a[1]-a[0])-(b[1]-b[0]));
+        Arrays.sort(costs,(a,b)->(b[1]-b[0])-(a[1]-a[0]));
         int n=costs.length/2;
         for(int i=0;i<n;i++)
         {
-            cost+=costs[i][1];
+            cost+=costs[i][0];
         }
         for(int i=n;i<costs.length;i++)
         {
-            cost+=costs[i][0];
+            cost+=costs[i][1];
         }
         return cost;
     }
