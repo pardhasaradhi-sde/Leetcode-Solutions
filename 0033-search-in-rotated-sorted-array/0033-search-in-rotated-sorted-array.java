551991f7cda9f301b1dad14cb55d578f2d4acbc3
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-05 18:49:11 +0530

    Time: 0 ms (100%), Space: 42.6 MB (6.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,38 @@
 class Solution {
-    public int search(int[] nums, int target) {
-       int low=0;
-       int high=nums.length-1;
-       while(low<=high)
-       {
-        int mid=low+(high-low)/2;
+    public int binsearch(int[] nums,int target,int left,int right)
+    {
+        if(left>right)
+        {
+            return -1;
+        }
+        int mid=left+(right-left)/2;
         if(nums[mid]==target)
         {
             return mid;
         }
-        else if(nums[low]<=nums[mid])
-        {
-            if(nums[low]<=target && nums[mid]>target)
-            {
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
         else{
-            if(nums[mid]<target && nums[high]>=target)
+            if(nums[left]<=nums[mid])
             {
-                low=mid+1;
+                if(nums[left]<=target && nums[mid]>=target)
+                {
+                    return binsearch(nums,target,left,mid-1);
+                }
+                else{
+                    return binsearch(nums,target,mid+1,right);
+                }
             }
             else{
-                high=mid-1;
+                if(nums[mid]<=target && nums[right]>=target)
+                {
+                    return binsearch(nums,target,mid+1,right);
+                }
+                else{
+                    return binsearch(nums,target,left,mid-1);
+                }
             }
         }
-       }
-        return -1;
+    }
+    public int search(int[] nums, int target) {
+        return binsearch(nums,target,0,nums.length-1);
     }
 }
