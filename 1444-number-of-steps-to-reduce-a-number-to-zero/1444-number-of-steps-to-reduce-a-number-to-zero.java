cbd2c6a3f289a11ab4f420a0b68ffa4cef3491f2
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-05 17:47:29 +0530

    Time: 0 ms (100%), Space: 40.9 MB (11.14%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,20 +1,18 @@
 class Solution {
-    public int count(int num)
-    {
-        if(num==0)
-        {
-            return 0;
-        }
-        if(num%2==0)
-        {
-            return 1+count(num/2);
-        }
-        else{
-            num-=1;
-            return 1+count(num);
-        }
-    }
     public int numberOfSteps(int num) {
-        return count(num); 
+        int count=0;
+        while(num!=0)
+        {
+            if(num%2==0)
+            {
+                num/=2;
+                count=count+1;
+            }
+            else{
+                num-=1;
+                count=count+1;
+            }
+        }
+        return count;
     }
 }
