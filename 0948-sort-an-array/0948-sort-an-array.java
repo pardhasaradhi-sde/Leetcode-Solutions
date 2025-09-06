ed467cbf342de554564f5cfef1bfcb8baa91c7d1
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-06 17:04:21 +0530

    Time: 23 ms (75.27%), Space: 56.4 MB (41.22%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,53 +1,43 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
-        mergeSort(nums,left,right);
-        return nums;
+    public static void merge(int[] arr,int[]temp,int left,int mid,int right)
+    {
+        for(int i=left;i<=right;i++)
+        {
+            temp[i]=arr[i];
+        }
+        int i=left,j=mid+1,k=left;
+        while(i<=mid && j<=right)
+        {
+            if(temp[i]>temp[j])
+            {
+                arr[k++]=temp[j++];
+            }
+            else{
+                arr[k++]=temp[i++];
+            }
+        }
+        while(i<=mid)
+        {
+            arr[k++]=temp[i++];
+        }
+        while(j<=right)
+        {
+            arr[k++]=temp[j++];
+        }
     }
-    public void mergeSort(int[] nums,int left,int right)
+    public static void mergesort(int[] nums,int[] temp,int left,int right)
     {
         if(left<right)
         {
             int mid=left+(right-left)/2;
-            mergeSort(nums,left,mid);
-            mergeSort(nums,mid+1,right);
-            merge(nums,left,mid,right);
+            mergesort(nums,temp,left,mid);
+            mergesort(nums,temp,mid+1,right);
+            merge(nums,temp,left,mid,right);
         }
     }
-    public void merge(int nums[],int left,int mid,int right)
-    {
-        int n1=mid-left+1;
-        int n2=right-mid;
-        int leftar[]=new int[n1];
-        int rightar[]=new int[n2];
-        for(int i=0;i<n1;i++)
-        {
-            leftar[i]=nums[left+i];
-        }
-        for(int j=0;j<n2;j++)
-        {
-            rightar[j]=nums[mid+1+j];
-        }
-        int i=0,j=0,k=left;
-        while(i<n1 && j<n2)
-        {
-            if(leftar[i]<=rightar[j])
-            {
-                nums[k++]=leftar[i++];
-            }
-            else
-            {
-                nums[k++]=rightar[j++];
-            }
-        }
-        while(i<n1)
-        {
-            nums[k++]=leftar[i++];
-        }
-         while(j<n2)
-        {
-            nums[k++]=rightar[j++];
-        }
+    public int[] sortArray(int[] nums) {
+        int temp[]=new int[nums.length];
+        mergesort(nums,temp,0,nums.length-1);
+        return nums;
     }
 }
