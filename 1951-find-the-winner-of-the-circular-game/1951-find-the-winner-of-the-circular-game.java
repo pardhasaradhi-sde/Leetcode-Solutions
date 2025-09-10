ef1ea9e70d8a3d1ede36dd6fa8e9ffb397b8d8a6
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-10 19:07:48 +0530

    Time: 32 ms (22.23%), Space: 44.9 MB (6.41%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,17 +1,20 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        ArrayList<Integer> l1=new ArrayList<>();
+        ArrayDeque<Integer> l1=new ArrayDeque<>();
         for(int i=1;i<=n;i++)
         {
-            l1.add(i);
+            l1.offer(i);
         }
-        int idx=0;
         while(l1.size()>1)
         {
-            idx=(idx+k-1)%l1.size();
-            l1.remove(idx);
+            for(int i=0;i<k-1;i++)
+            {
+                int ele=l1.removeFirst();
+                l1.addLast(ele);
+            }
+            l1.removeFirst();
         }
-        return l1.get(0);
+        return l1.removeFirst();
         
     }
 }
