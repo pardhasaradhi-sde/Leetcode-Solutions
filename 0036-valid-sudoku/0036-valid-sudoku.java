5eefac66374520f1122cf5c8b64652297db5e4db
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-14 23:42:02 +0530

    Time: 2 ms (63.71%), Space: 44.2 MB (95.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,69 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean rows[][]=new boolean[9][9];
-        boolean cols[][]=new boolean[9][9];
-        boolean boxes[][]=new boolean[9][9];
-        for(int i=0;i<9;i++)
+        int m=board.length;
+        int n=board[0].length;
+       for(int i=0;i<m;i++)
+       {
+        Set<Character> s1=new HashSet<>();
+        for(int j=0;j<n;j++)
         {
-            for(int j=0;j<9;j++)
+            if(board[i][j]=='.')
             {
-                char c=board[i][j];
-                if(c=='.')
-                {
-                    continue;
-                }
-                int num=c-'1';
-                int box=(i/3)*3+(j/3);
-                if(rows[i][num] || cols[j][num] || boxes[box][num])
-                {
-                    return false;
-                }
-                rows[i][num]=cols[j][num]=boxes[box][num]=true;
+                continue;
+            }
+            if(s1.contains(board[i][j]))
+            {
+                return false;
+            }
+            s1.add(board[i][j]);
+        }
+       }
+       for(int i=0;i<n;i++)
+       {
+        Set<Character> s1=new HashSet<>();
+        for(int j=0;j<m;j++)
+        {
+            if(board[j][i]=='.')
+            {
+                continue;
+            }
+            if(s1.contains(board[j][i]))
+            {
+                return false;
+            }
+            s1.add(board[j][i]);
+        }
+       }
+       for(int sr=0;sr<m;sr+=3)
+       {
+        int er=sr+2;
+        for(int sc=0;sc<n;sc+=3)
+        {
+            int ec=sc+2;
+            if(!isvalid(board,sr,er,sc,ec))
+            {
+                return false;
+            }
+        }
+       }
+       return true;
+    }
+    public boolean isvalid(char[][] board,int sr,int er,int sc,int ec)
+    {
+        Set<Character> s1=new HashSet<>();
+        for(int i=sr;i<=er;i++)
+        {
+            for(int j=sc;j<=ec;j++)
+            {
+                if(board[i][j]=='.')
+            {
+                continue;
+            }
+            if(s1.contains(board[i][j]))
+            {
+                return false;
+            }
+            s1.add(board[i][j]);
             }
         }
         return true;
