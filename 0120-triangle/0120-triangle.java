6551290acba2cf58cdd5e88f220c8c3fe8d2edda
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-20 15:35:37 +0530

    Time: 5 ms (35.61%), Space: 45.7 MB (20.77%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        int dp[][]=new int[n][n];
+        for(int i=0;i<dp.length;i++)
+        {
+            dp[n-1][i]=triangle.get(n-1).get(i);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int plusone=triangle.get(i).get(j)+dp[i+1][j];
+                int one=triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(plusone,one);
+            }
+        }
+        return dp[0][0];
+    }
+}
