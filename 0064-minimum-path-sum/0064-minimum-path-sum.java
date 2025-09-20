5888d69ef857f8322d0b60bde58833655ddd3b43
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-20 14:35:05 +0530

    Time: 1 ms (99.75%), Space: 47.9 MB (21.8%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solve(int row,int col,int rows,int cols,int[][] grid,int dp[][])
+    {
+         if(row==rows-1 && col==cols-1)
+        {
+            return grid[rows-1][cols-1];
+        }
+        if(row>=rows || col>=cols)
+        {
+            return Integer.MAX_VALUE;
+        }
+        if(dp[row][col]!=-1)
+        {
+            return dp[row][col];
+        }
+        int down=solve(row+1,col,rows,cols,grid,dp);
+        int right=solve(row,col+1,rows,cols,grid,dp);
+        return dp[row][col]=grid[row][col]+Math.min(down,right);
+    }
+    public int minPathSum(int[][] grid) {
+        int dp[][]=new int[grid.length][grid[0].length];
+        for(int i=0;i<dp.length;i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(0,0,grid.length,grid[0].length,grid,dp);
+    }
+}
