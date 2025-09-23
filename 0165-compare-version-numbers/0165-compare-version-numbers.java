c9a159de43055a2bdb2f03c71114e2bc5d64aec8
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-23 06:03:49 +0530

    Time: 1 ms (76.03%), Space: 41.7 MB (32.01%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String v1[]=version1.split("\\.");
+        String v2[]=version2.split("\\.");
+        int maxlen=Math.max(v1.length,v2.length);
+        int num1=0;
+        int num2=0;
+        for(int i=0;i<maxlen;i++)
+        {
+            num1=(i<v1.length)?Integer.parseInt(v1[i]):0;
+            num2=(i<v2.length)?Integer.parseInt(v2[i]):0;
+            if(num1<num2)
+            {
+                return -1;
+            }
+            if(num1>num2)
+            {
+                return 1;
+            }
+        }
+        return 0;
+
+    }
+}
