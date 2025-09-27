11a3041d11c1c1c865078de2938c44de9ce7e9dd
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-27 12:31:11 +0530

    Time: 1 ms (90.36%), Space: 44.9 MB (7.23%) - LeetHub

---
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean satisfiesConditions(int[][] grid) {
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(i<grid.length-1)
+                {
+                    if(grid[i][j]!=grid[i+1][j])
+                    {
+                        return false;
+                    }
+                }
+                if(j<grid[0].length-1)
+                {
+                    if(grid[i][j] == grid[i][j + 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
