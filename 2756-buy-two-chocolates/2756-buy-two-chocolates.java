fd5954238c85e74569f02051166400e86d9b7c16
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-27 13:30:40 +0530

    Time: 1 ms (100%), Space: 44.5 MB (55.5%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -1,11 +1,20 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-            if(prices[0]+prices[1]<=money)
+        int chocolate1=Integer.MAX_VALUE;
+        int chocolate2=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++)
+        {
+            int val=prices[i];
+            if(val<chocolate1)
             {
-                money-=prices[0];
-                money-=prices[1];
+                chocolate2=chocolate1;
+                chocolate1=val;
             }
-        return money;
+            else if(val<chocolate2){
+                chocolate2=val;
+            }
+        }
+        int cost=money-(chocolate1+chocolate2);
+        return cost>=0?cost:money;
     }
 }
