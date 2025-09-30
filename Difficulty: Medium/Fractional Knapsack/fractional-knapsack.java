8eaab723e1451a363cc5e93384a7713534c7eead
SEERAPU PARDHA SARADHI <24eg505j02@anurag.edu.in>
2025-09-30 12:13:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
@@ -1,29 +1,29 @@
 class Solution {
     public double fractionalKnapsack(int[] val, int[] wt, int capacity) {
-        int n=val.length;
-        Double []ratio=new Double[val.length];
-        Integer []idx=new Integer[val.length];
-        for(int i=0;i<val.length;i++)
-        {
-            ratio[i]=(double)val[i]/wt[i];
-            idx[i]=i;
-        }
-        Arrays.sort(idx,(a,b)->Double.compare(ratio[b],ratio[a]));
-        double score=0.0;
-        for(int i=0;i<val.length;i++)
-        {
-            int j=idx[i];
-            if(wt[j]<=capacity)
-            {
-                score+=val[j];
-                capacity-=wt[j];
-            }
-            else{
-                double fraction=(double)capacity/wt[j];
-                score+=val[j]*fraction;
-                break;
-            }
-        }
-        return score;
+       int n=val.length;
+       Double[] ratio=new Double[n];
+       Integer[] idx=new Integer[n];
+       for(int i=0;i<n;i++)
+       {
+           ratio[i]=(double)val[i]/wt[i];
+           idx[i]=i;
+       }
+       Arrays.sort(idx,(a,b)->Double.compare(ratio[b],ratio[a]));
+       double score=0.0;
+       for(int i=0;i<n;i++)
+       {
+           int j=idx[i];
+           if(wt[j]<=capacity)
+           {
+               score+=val[j];
+               capacity-=wt[j];
+           }
+           else{
+               double fraction=(double)capacity/wt[j];
+               score+=val[j]*fraction;
+               break;
+           }
+       }
+       return score;
     }
 }
